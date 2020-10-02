248b10b826a73b0b4b1e5cc92f6fe7883b9dd9ae
Quinton Bryant <Aivorlan@icloud.com>
2020-10-01 23:51:08 -0500

    defining and exporting classes

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,31 @@
 // TODO: Write code to define and export the Employee class
+class Employee {
+    constructor(name, id, role, githubuname, school) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+        this.role = role;
+        this.githubuname = githubuname;
+        this.school = school;
+    }
+    getName() {
+        return this.name;
+    };
+    getId() {
+        return this.id;
+    };
+    getEmail(){
+        return this.email;
+    }
+    getRole() {
+        return this.role;
+    }
+    getGhusername() {
+        return this.githubuname;
+    }
+    getSchool() {
+        return this.school;
+    }
+}
+
+module.exports = Employee;
